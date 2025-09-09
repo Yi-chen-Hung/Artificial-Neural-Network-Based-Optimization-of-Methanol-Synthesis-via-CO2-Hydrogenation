9e01ce5ef9dd4d80564e06d0dc4069c612c708b1
Yi-chen-Hung <andyhung0320@gmail.com>
2025-09-09 15:15:56 +0800

    Update Feature selection and correaltion.py

--- Feature selection and correaltion.py
+++ Feature selection and correaltion.py
@@ -13,10 +13,5 @@ for target in targets:
     globals()[f"corr_{n}"]=pd.DataFrame(corr[target][np.abs(corr[target])>0.5])
     corr_comp=pd.concat([corr_comp, globals()[f"corr_{n}"]], axis=1)
     n+=1
-# Optionally, visualize correlations
-#plt.figure(figsize=(10,6))
-#sns.heatmap(corr[targets], annot=True, cmap="coolwarm", center=0)
-#plt.title("Correlation of Features with Targets")
-#plt.show()
 Possible_para=corr_comp[~corr_comp.index.str.contains("STREAMS")].fillna(0) #remove the mass/mole flow which is certainly related with target parameter
 Possible_para
