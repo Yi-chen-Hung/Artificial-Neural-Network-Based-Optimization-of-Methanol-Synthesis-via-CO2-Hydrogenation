620de192fb2826ec90324a552d99dd1f8b80f3f3
Yi-chen-Hung <andyhung0320@gmail.com>
2025-11-15 21:45:13 +0100

    Update Feature selection and correaltion.py

--- Feature selection and correaltion.py
+++ Feature selection and correaltion.py
@@ -1,17 +1,23 @@
 import pandas as pd
 import numpy as np
-Fdata_profil=pd.read_excel('Fdata_profile.xlsx').dropna().drop(columns=['Unnamed: 0'])
+for typ in ['insert_up','osci','profil','rauf','runter']:
+    globals()[f"Fdata_{typ}"]=pd.read_excel(f'Fdata_{typ}.xlsx').dropna() # read different hydrogen input type raw data 
 #--------------------------------------------------------------------------------------------------------------------------------------------
-targets = ['Methanol_lag', 'CO2_lag', 'CO_lag']
-corr = Fdata_profil[Fdata_profil.columns.drop(list(Fdata_profil.filter(regex='%')))].drop(columns=['Time']).corr()[targets]
-corr1=corr[~corr.index.str.contains('lag')].dropna()
-
-def Correlation_05(df,parameter):
+# Connect two differen pattern of Hydrogen input data 
+Fdata_total=pd.DataFrame()
+for typ in ['profil','osci']:
+    Fdata_total=pd.concat([Fdata_total,globals()[f"Fdata_{typ}"]],axis=0)
+Fdata_total=Fdata_total.reset_index(drop=True)
+#--------------------------------------------------------------------------------------------------------------------------------------------
+# calculate correlationship between target parameter and every other columns
+def Correlation(df,parameter):
     list1=list(df.sort_values(parameter, ascending=True)[parameter][np.abs(df[parameter])>0.5].index)
     return list1
-
-selected_features=list(set(Correlation_05(corr1,'Methanol_lag')).union(Correlation_05(corr1,'CO2_lag')).union(Correlation_05(corr1,'CO_lag')))
-len(selected_features)
+targets = ['Methanol_lag', 'CO2_lag', 'CO_lag']
+corr = Fdata_total[Fdata_total.columns.drop(list(Fdata_total.filter(regex='%')))].drop(columns=['Time']).corr()[targets]
+corr1=corr[~corr.index.str.contains('lag')].dropna()
+selected_features=list(set(Correlation(corr1,'Methanol_lag')).union(Correlation(corr1,'CO2_lag')).union(Correlation(corr1,'CO_lag')))
+print("Selected_feature:",len(selected_features))
 #--------------------------------------------------------------------------------------------------------------------------------------------
 # Target parameters PCA analysis
 import seaborn as sns
@@ -19,7 +25,7 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
-PCA_df=Fdata_profil[selected_features].loc[2:,:].dropna()
+PCA_df=Fdata_total[selected_features]
 
 X_scaled = scaler.fit_transform(PCA_df)
 pca = PCA()
